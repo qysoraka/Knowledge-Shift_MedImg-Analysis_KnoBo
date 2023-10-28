70509aac1a08df7e63bcd85f7d5c621e11e0eb4e
qysoraka <156439778+qysoraka@users.noreply.github.com>
2023-10-28 22:39:35 +0000

    vm

--- modules/models.py
+++ modules/models.py
@@ -3,4 +3,12 @@ import torch.nn as nn
 
 class LogisticRegressionT(torch.nn.Module):
     def __init__(self, input_dim, output_dim):
-        super(LogisticRegression
+        super(LogisticRegressionT, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        outputs = torch.sigmoid(self.linear(x))
+        return outputs
+
+
+class MultiLabelModel(nn.Modul
