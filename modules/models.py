ba6e7453b686982edb7527f814fca9070bb444e8
qysoraka <156439778+qysoraka@users.noreply.github.com>
2023-12-01 14:43:04 +0000

    Release 0.3.1

--- modules/models.py
+++ modules/models.py
@@ -28,4 +28,12 @@ class MultiClassLogisticRegression(nn.Module):
     def __init__(self, num_features, num_classes, prior, apply_prior=False):
         super(MultiClassLogisticRegression, self).__init__()
         self.linear = nn.Linear(num_features, num_classes)
-        self.prior = prior.cuda() # prior is a weight matrix has the same shape as the w
+        self.prior = prior.cuda() # prior is a weight matrix has the same shape as the weight matrix of the linear layer
+        self.apply_prior = apply_prior
+    
+    def forward(self, x):
+        return self.linear(x)
+
+
+class PosthocHybridCBM(nn.Module):
+    def __init__(self, n_concepts, n_classes, n_image_features, apply_pri
